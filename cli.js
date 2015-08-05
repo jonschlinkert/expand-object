f9c629c599737787b8d88de15c44fee9ab6a0dcd
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-05 03:45:10 -0400

    use minimist

--- cli.js
+++ cli.js
@@ -1,34 +1,57 @@
 #!/usr/bin/env node
 
 'use strict';
-var program = require('commander');
-var stdin = require('get-stdin');
-var expand = require('./index');
 
 process.title = 'expand-object';
 
+var argv = require('minimist')(process.argv.slice(2));
+var stdin = require('get-stdin');
+var Store = require('data-store');
+var store = new Store('expand-object');
+
+var expand = require('./index');
 var pkg = require('./package.json');
 
-program
-  .description(pkg.description)
-  .version(pkg.version)
-  .usage('[options] <string>')
-  .option('-r, --raw', "output as raw javascript object - don't stringify")
-  .on('--help', function() {
-    console.log('  Examples:');
-    console.log('');
-    console.log('    $ expand-object "a:b"');
-    console.log('    $ expand-object --raw "a:b"');
-    console.log('    $ echo "a:b" | expand-object');
-    console.log('');
-  })
-  .parse(process.argv);
+function help() {
+  console.log();
+  console.log('  ' + pkg.description);
+  console.log();
+  console.log('  Usage: cli [options] <string>');
+  console.log();
+  console.log('    -h, --help     output usage information');
+  console.log('    -V, --version  output the version number');
+  console.log('    -r, --raw      output as raw javascript object - don\'t stringify');
+  console.log();
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ expand-object "a:b"');
+  console.log('    $ expand-object --raw "a:b"');
+  console.log('    $ echo "a:b" | expand-object');
+  console.log('');
+}
+
+if (argv._.length === 0 && Object.keys(argv).length === 1 || argv.help) {
+  help();
+}
 
 function run(contents) {
-  var output = expand(contents);
-  if (!program.raw) {
-    output = JSON.stringify(output);
+  if (argv.get) {
+    output = store.get(argv.get);
+    console.log(output);
+    return;
   }
+
+  var output = contents;
+  if (argv.set) {
+    output = expand(argv.set);
+    store.set(output);
+  } else {
+    output = expand(contents);
+    if (!argv.raw) {
+      output = JSON.stringify(output);
+    }
+  }
+
   console.log(output);
   process.exit(0);
 }
@@ -39,9 +62,4 @@ if (!process.stdin.isTTY) {
   });
 }
 
-if (!program.args.length) {
-  program.help();
-  return;
-}
-
-run(program.args[0]);
+run(argv._[0] || '');
